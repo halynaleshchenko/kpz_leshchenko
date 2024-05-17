c76241cb4c42c1668e0f5e7d844141120d3bd482
Galina Leshchenko <ipz222_lgo@student.ztu.edu.ua>
2024-05-18 00:01:17 +0300

    +Command Pattern for Node Manipulation

--- Lab_5/Console5/Program.cs
+++ Lab_5/Console5/Program.cs
@@ -12,6 +12,7 @@ namespace Console5
         private static void Main(string[] args)
         {
             Feature1();
+            Feature2();
         }
 
         private static void Feature1()
@@ -33,6 +34,31 @@ namespace Console5
             {
                 Console.WriteLine(node.OuterHTML);
             }
+        }
+        private static void Feature2()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Feature2:");
+            Console.ResetColor();
+
+            var root = new LightElementNode("div", "", "", new List<string>(), new List<LightNode>
+            {
+                new LightTextNode("Hello, world!"),
+                new LightElementNode("span", "", "", new List<string>{"class1"}, new List<LightNode>
+                {
+                    new LightTextNode("Nested span")
+                })
+            });
+
+            var newChild = new LightTextNode("New Child");
+            var addChildCommand = new AddChildCommand(root, newChild);
+            addChildCommand.Execute();
+
+            var removeChildCommand = new RemoveChildCommand(root, newChild);
+            removeChildCommand.Execute();
+            removeChildCommand.Undo();
+
+            Console.WriteLine(root.OuterHTML);
         }
     }
 }
---
+++ Lab_5/Console5/RemoveChildCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console5
+{
+    public class RemoveChildCommand : ICommand
+    {
+        private LightElementNode parent;
+        private LightNode child;
+
+        public RemoveChildCommand(LightElementNode parent, LightNode child)
+        {
+            this.parent = parent;
+            this.child = child;
+        }
+
+        public void Execute()
+        {
+            parent.ChildNodes.Remove(child);
+        }
+
+        public void Undo()
+        {
+            parent.ChildNodes.Add(child);
+        }
+    }
+}
