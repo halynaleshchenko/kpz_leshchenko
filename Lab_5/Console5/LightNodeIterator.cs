f188591417b099e2516fdca08495f9b8dd8500a5
Galina Leshchenko <ipz222_lgo@student.ztu.edu.ua>
2024-05-17 23:13:40 +0300

    +Iterator for iterating all HTML

---
+++ Lab_5/Console5/LightNodeIterator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console5
+{
+    public class LightNodeIterator : IEnumerable<LightNode>
+    {
+        private LightNode rootNode;
+
+        public LightNodeIterator(LightNode rootNode)
+        {
+            this.rootNode = rootNode;
+        }
+
+        public IEnumerator<LightNode> GetEnumerator()
+        {
+            return TraverseInDepth(rootNode).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private IEnumerable<LightNode> TraverseInDepth(LightNode node)
+        {
+            yield return node;
+            if (node is LightElementNode elementNode)
+            {
+                foreach (var child in elementNode.ChildNodes)
+                {
+                    foreach (var descendant in TraverseInDepth(child))
+                    {
+                        yield return descendant;
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<LightNode> TraverseInWidth(LightNode node)
+        {
+            Queue<LightNode> queue = new Queue<LightNode>();
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                yield return current;
+                if (current is LightElementNode elementNode)
+                {
+                    foreach (var child in elementNode.ChildNodes)
+                    {
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lab_5/Console5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Console5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Feature1();
+        }
+
+        private static void Feature1()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Feature1:");
+            Console.ResetColor();
+            var root = new LightElementNode("div", "", "", new List<string>(), new List<LightNode>
+            {
+                new LightTextNode("Hello, world!"),
+                new LightElementNode("span", "", "", new List<string>{"class1"}, new List<LightNode>
+                {
+                    new LightTextNode("Nested span")
+                })
+            });
+
+            var iterator = new LightNodeIterator(root);
+            foreach (var node in iterator)
+            {
+                Console.WriteLine(node.OuterHTML);
+            }
+        }
+    }
+}
