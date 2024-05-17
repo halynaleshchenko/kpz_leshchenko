8fcd455c465bfe2c89e595d8894c4fcf9c5996ce
Galina Leshchenko <ipz222_lgo@student.ztu.edu.ua>
2024-05-17 22:21:49 +0300

    the beginning of the Lab_5

---
+++ Lab_5/Console5/LightTextNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console5
+{
+    public class LightTextNode : LightNode
+    {
+        private readonly string _text;
+
+        public LightTextNode(string text)
+        {
+            _text = text;
+        }
+
+        public override string OuterHTML => _text;
+
+        public override string InnerHTML => _text;
+
+        public string Text => _text;
+    }
+}
